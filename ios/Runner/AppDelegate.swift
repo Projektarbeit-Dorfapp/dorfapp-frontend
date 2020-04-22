89fb90231622f684ed09032d19be13b3a9d0563f
Phillwich <p.hellwich@gmx.de>
2020-04-22 09:44:26 +0200

    firebase inport pod file

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,9 +1,9 @@
 import UIKit
 import Flutter
+import Firebase
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate, UIApplicationDelegate {
-  var window: UIWindow?
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
