6d2f869837301749622ace342f43dd4bb7b82947
Philipp <P.Hellwich@gmx.de>
2020-06-07 21:54:34 +0200

    :sparkels: hier ist der shit

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,15 +1,14 @@
 import UIKit
 import Flutter
-import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-    override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-      // Override point for customization after application launch.
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
 
-      return true
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-    override init() {
-        FirebaseApp.configure()
-    }
 }
