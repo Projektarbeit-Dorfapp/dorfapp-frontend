34fef7ca9349b1f55a24a6c43ebe6cea413eb171
KilianBerthold <63092817+KilianBerthold@users.noreply.github.com>
2020-09-16 10:11:28 +0200

    :sparkles: Sorting of comments in "News Detail" and forum posts

--- lib/widgets/comment_section.dart
+++ lib/widgets/comment_section.dart
@@ -1,9 +1,11 @@
 import 'dart:developer';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dorf_app/constants/menu_buttons.dart';
 import 'package:dorf_app/models/comment_model.dart';
 import 'package:dorf_app/models/topComment_model.dart';
 import 'package:dorf_app/screens/general/empty_list_text.dart';
+import 'package:dorf_app/screens/general/textNoteBar.dart';
 import 'package:dorf_app/screens/login/loginPage/provider/accessHandler.dart';
 import 'package:dorf_app/services/alert_service.dart';
 import 'package:dorf_app/services/comment_service.dart';
@@ -15,24 +17,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 
-
 //Meike Nedwidek
 class CommentSection extends StatefulWidget {
   final List<TopComment> commentList;
   final String document;
   final String collection;
   final bool disableAddingComment;
-  final SubscriptionType subscriptionType; ///where is the commentsection implemented? [SubscriptionType.news] when in news or [SubscriptionType.entry] when in Forum
+  final SubscriptionType subscriptionType;
+
+  ///where is the commentsection implemented? [SubscriptionType.news] when in news or [SubscriptionType.entry] when in Forum
 
   CommentSection(this.commentList, this.document, this.collection, this.subscriptionType, {this.disableAddingComment});
 
   @override
-  _CommentSectionState createState() =>
-      _CommentSectionState();
+  _CommentSectionState createState() => _CommentSectionState();
 }
 
 class _CommentSectionState extends State<CommentSection> {
-
   final commentService = CommentService();
   TextEditingController _controller;
   String answerTo;
@@ -60,15 +61,12 @@ class _CommentSectionState extends State<CommentSection> {
     super.dispose();
   }
 
-  Future<void> _addNewComment(String val, String answerTo) async{
+  Future<void> _addNewComment(String val, String answerTo) async {
     AccessHandler _accessHandler = Provider.of<AccessHandler>(context, listen: false);
     var user = await _accessHandler.getUser();
     var date = Timestamp.now();
     Comment newComment = Comment(
-        user: User(
-            firstName: user.firstName,
-            lastName: user.lastName,
-            uid: user.uid),
+        user: User(firstName: user.firstName, lastName: user.lastName, uid: user.uid),
         content: val,
         createdAt: date,
         modifiedAt: date);
@@ -78,57 +76,112 @@ class _CommentSectionState extends State<CommentSection> {
       setState(() {
         curCommentList.firstWhere((element) => element.comment.id == answerTo).answerList.add(newComment);
       });
-    }
-    else {
-      commentService.insertNewComment(
-          widget.document, widget.collection, newComment, Provider.of<AlertService>(context, listen: false), widget.subscriptionType);
+    } else {
+      commentService.insertNewComment(widget.document, widget.collection, newComment,
+          Provider.of<AlertService>(context, listen: false), widget.subscriptionType);
 
       setState(() {
         TopComment newTopComment = TopComment(newComment, []);
         curCommentList.insert(0, newTopComment);
       });
     }
-
   }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: EdgeInsets.only(left: 20, right: 20),
-        child: Column(
-          children: <Widget>[
-            widget.disableAddingComment != true ? TextField(
-              focusNode: myFocusNode,
-              controller: _controller,
-              onChanged: (text) {
-                if(text.isEmpty) {
-                  answerTo = null;
-                }
-              },
-              onSubmitted: (String submittedStr) async {
-                await _addNewComment(submittedStr, answerTo);
-                //Provider.of<UpdateCommentCard>(context, listen: false).update();
-                answerTo = null;
-                _controller.clear();
-              },
-              decoration: InputDecoration(
-              contentPadding: const EdgeInsets.all(20.0),
-              hintText: "Schreibe einen Kommentar...",
-            ),
-            ) : Container(),
-            Column(
-              children: <Widget>[
-                Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: curCommentList.length > 0
-                        ? curCommentList
-                        .map((comment) => CommentCard(topComment: comment, collection: widget.collection, document: widget.document, disableAddingComment: widget.disableAddingComment, emitAnswerTo: function))
-                        .toList()
-                        : [ShowTextIfListEmpty(iconData: Icons.message, text: "Noch keine Kommentare",)]),
-              ],
-            )
-          ],
-        ),
+      padding: EdgeInsets.only(left: 20, right: 20),
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: [
+              Expanded(
+                child: widget.disableAddingComment != true
+                    ? TextField(
+                        focusNode: myFocusNode,
+                        controller: _controller,
+                        onChanged: (text) {
+                          if (text.isEmpty) {
+                            answerTo = null;
+                          }
+                        },
+                        onSubmitted: (String submittedStr) async {
+                          await _addNewComment(submittedStr, answerTo);
+                          //Provider.of<UpdateCommentCard>(context, listen: false).update();
+                          answerTo = null;
+                          _controller.clear();
+                        },
+                        decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.all(20.0),
+                          hintText: "Schreibe einen Kommentar...",
+                        ),
+                      )
+                    : TextNoteBar(
+                        text: "Der Kommentarbereich wurde geschlossen.",
+                        backgroundColor: Colors.white24,
+                      ),
+              ),
+              PopupMenuButton(
+                icon: Icon(Icons.tune, color: Theme.of(context).primaryColor),
+                onSelected: (value) {
+                  FocusManager.instance.primaryFocus.unfocus();
+                  var tempCommentList = curCommentList;
+                  switch (value) {
+                    case MenuButtons.SORT_ASCENDING:
+                      {
+                        tempCommentList.sort((a, b) => (a.comment.createdAt.compareTo(b.comment.createdAt)));
+                      }
+                      break;
+                    case MenuButtons.SORT_DESCENDING:
+                      {
+                        tempCommentList.sort((a, b) => (b.comment.createdAt.compareTo(a.comment.createdAt)));
+                      }
+                      break;
+                    default:
+                      {
+                        tempCommentList.sort((a, b) => (b.comment.createdAt.compareTo(a.comment.createdAt)));
+                      }
+                      break;
+                  }
+                  setState(() {
+                    curCommentList = tempCommentList;
+                  });
+                },
+                color: Colors.white,
+                itemBuilder: (BuildContext context) {
+                  return MenuButtons.NewsSorting.map((String choice) {
+                    return PopupMenuItem<String>(
+                      value: choice,
+                      child: Text(choice),
+                    );
+                  }).toList();
+                },
+              )
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: curCommentList.length > 0
+                      ? curCommentList
+                          .map((comment) => CommentCard(
+                              topComment: comment,
+                              collection: widget.collection,
+                              document: widget.document,
+                              disableAddingComment: widget.disableAddingComment,
+                              emitAnswerTo: function))
+                          .toList()
+                      : [
+                          ShowTextIfListEmpty(
+                            iconData: Icons.message,
+                            text: "Noch keine Kommentare",
+                          )
+                        ]),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
